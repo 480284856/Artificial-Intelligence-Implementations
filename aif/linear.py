445fc255b22d618f15add42cf2a14208136c128f
Jay <480284856@qq.com>
2026-05-01 11:40:37 +0200

    Apply parameter class and implement the back propagation of linear layer

--- aif/linear.py
+++ aif/linear.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .utils.module import Module
+from .utils.module import Module, Parameter
 
 class Linear(Module):
     def __init__(self, in_features: int, out_features:int, bias:bool=True, *args, **kwargs):
@@ -7,11 +7,17 @@ class Linear(Module):
         self.out_features = out_features
         self.set_bias = bias
 
-        self.W = np.random.randn(self.in_features, self.out_features)
-        self.B = np.random.randn(self.out_features) if self.set_bias else 0
+        self.W = Parameter(np.random.randn(self.in_features, self.out_features))
+        self.B = Parameter(np.random.randn(self.out_features) if self.set_bias else np.zeros(1))
+
+        self.parameters = dict(
+            W=self.W,
+            B=self.B
+        )
 
     def forward(self, X: np.ndarray):
-        return X@self.W+self.B
+        self.X=X
+        return X@self.W.value+self.B.value
     
     def backward(self, delta: np.ndarray):
         """
@@ -20,11 +26,15 @@ class Linear(Module):
         :param delta: $delta=\\frac{ \partial L }{ \partial Z_{L+1} }$
         :type delta: np.ndarray
         """
-        # gradient
-        
-
-        
         # delta: [bs, out_features]
         # W: [in_features, out_features]
+        # X: [bs, in_features]
+
+        # gradient
+        self.W.grad = self.X.T @ delta
+        # 对于偏置的求导来说，它就等于那一层的 delta 乘以输入（也就是 1），其实也就是 delta。因为又是批量的（batch size 大于 1），所以会有多个 delta，最后取平均。
+        assert len(delta.shape)==2, "对偏置求导的假设是基于：第一个维度是批大小维度，第二个维度则是特征维度，也就是神经元的个数的维度"
+        self.B.grad = delta.sum(dim=0)
+
         delta = delta @ self.W.T
         return delta
