b66f7d7a84922bacfa4278bf2b88af82037182e6
Jay <480284856@qq.com>
2026-06-24 20:28:57 +0200

    Pre-computation for the positional embedding.

--- aif/embedding.py
+++ aif/embedding.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .utils.module import TrainableModule, Parameter
+from .utils.module import TrainableModule, Parameter, Module
 
 class Embedding(TrainableModule):
     def __init__(self,
@@ -13,7 +13,7 @@ class Embedding(TrainableModule):
     def forward(self, X:np.ndarray):
         assert len(X.shape) == 1, "X = [1,2,3,...]"
         self.inputs = X.copy()
-        return self.embedding[X]
+        return self.embedding.value[X]
     
     def backward(self, delta):
         # delta = αL/α_output
@@ -22,3 +22,36 @@ class Embedding(TrainableModule):
         grad = np.zeros_like(self.embedding.value)
         np.add.at(grad, self.inputs, delta)
         self.embedding.grad = grad
+
+class PositionalEmbedding(Module):
+    def __init__(self, d_model: int = None, max_len: int = 5000):
+        super().__init__()
+        self.d_model = d_model
+        self.max_len = max_len
+        self.pe = None
+        if d_model is not None:
+            self._precompute_pe()
+            
+    def _precompute_pe(self):
+        pe = np.zeros((self.max_len, self.d_model))
+        pos = np.arange(self.max_len).reshape(-1,1)
+        idx_e = np.arange(0, self.d_model,step=2).reshape(1,-1)
+        idx_o = np.arange(1, self.d_model,step=2).reshape(1,-1)
+        tmp_e = pos/10000**(idx_e/self.d_model)
+        tmp_o = pos/10000**((idx_o - 1)/self.d_model)
+        pe[:, 0::2] = np.sin(tmp_e)
+        pe[:, 1::2] = np.cos(tmp_o)
+        
+        self.pe = pe
+
+    def forward(self, X: np.ndarray):
+        """
+        X: token embeddings
+           shape: (seq_len, d_model) or (batch_size, seq_len, d_model)
+        """
+        ...
+    def backward(self, delta: np.ndarray):
+        """
+        For X_out = X_in + PE, the gradient with respect to X_in is delta.
+        """
+        ...
